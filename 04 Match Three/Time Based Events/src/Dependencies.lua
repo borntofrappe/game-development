bda50f6f4ecc5de4a0dd67e8073ca4b43a5f3df5
gabrielecorti <borntofrappe@gmail.com>
2019-02-21 18:29:37 +0100

    complete the resources

---
+++ 04 Match Three/Time Based Events/src/Dependencies.lua
@@ -0,0 +1,9 @@
+-- require the libraries and files used throughout the application
+
+-- push, to create a 'projection' giving the game a retro, pixelated look
+push = require 'lib/push'
+-- class, to work with classes
+Class = require 'lib/class'
+
+-- constants specified in constants.lua
+require 'src/constants'
