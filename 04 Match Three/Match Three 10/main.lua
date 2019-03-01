a3bfb1df10b16e8e5bd32babfa27e6009b51e8c8
gabrielecorti <borntofrappe@gmail.com>
2019-03-01 23:52:22 +0100

    include lua files

---
+++ 04 Match Three/Match Three 10/main.lua
@@ -0,0 +1,117 @@
+-- require dependencies.lua, itself calling for the necessary resources
+require 'src/Dependencies'
+
+-- in the load function set up the screen with the push library
+function love.load()
+  -- title of the screen
+  love.window.setTitle('Match Three')
+  -- random seed to have math.random reference different values
+  math.randomseed(os.time())
+
+  -- GLOBALS
+  -- fonts
+  gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['normal'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['big'] = love.graphics.newFont('fonts/font.ttf', 24),
+    ['humongous'] = love.graphics.newFont('fonts/font.ttf', 56)
+  }
+
+  -- graphics
+  gTextures = {
+    ['background'] = love.graphics.newImage('graphics/background.png'),
+    ['spritesheet'] = love.graphics.newImage('graphics/match3.png')
+  }
+
+  -- assets created from the graphics
+  gFrames = {
+    ['tiles'] = GenerateQuads(gTextures['spritesheet'], 32, 32)
+  }
+
+  -- sounds
+  gSounds = {
+    ['clock'] = love.audio.newSource('sounds/clock.wav', 'static'),
+    ['game-over'] = love.audio.newSource('sounds/game-over.wav', 'static'),
+    ['match'] = love.audio.newSource('sounds/match.wav', 'static'),
+    ['next-level'] = love.audio.newSource('sounds/next-level.wav', 'static'),
+    ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+
+    ['music3'] = love.audio.newSource('sounds/music3.mp3', 'static')
+  }
+
+  -- STATE MACHINE
+  -- create an instance of the state machine, with the possible states of the game
+  gStateMachine = StateMachine {
+    ['start'] = function() return StartState() end,
+    ['play'] = function() return PlayState() end,
+    ['gameover'] = function() return GameoverState() end
+  }
+  -- immediately reference the start state
+  gStateMachine:change('start')
+
+
+  -- PUSH LIBRARY
+  -- virtualization through the push library
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = true,
+    vsync = true
+  })
+
+  -- variable allowing to have the background scroll
+  backgroundOffset = 0
+
+  -- table recording the different keys being pressed
+  love.keyboard.keyPressed = {}
+end
+
+-- RESIZE
+-- in the resize function update the resolution through the push library
+function love.resize(width, height)
+  push:resize(width, height)
+end
+
+
+-- KEYPRESSES
+-- in the keypressed function keep track of the key being pressed in keyPressed
+function love.keypressed(key)
+  love.keyboard.keyPressed[key] = true
+end
+
+-- create a function to check whether a specific key was recorded in the keyPressed table
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keyPressed[key]
+end
+
+-- UPDATE
+function love.update(dt)
+  -- play the soundtrack in a loop
+  gSounds['music3']:setLooping(true)
+  gSounds['music3']:play()
+
+  -- update backgroundOffset to have the background constantly scroll to the left
+  backgroundOffset = (backgroundOffset + BACKGROUND_OFFSET_SPEED * dt) % 512
+
+  -- update the game as per the state machine
+  gStateMachine:update(dt)
+
+  -- reset keyPressed to an empty table
+  love.keyboard.keyPressed = {}
+end
+
+-- DRAW
+function love.draw()
+  push:start()
+
+  -- include the background before every other graphic
+  love.graphics.draw(gTextures['background'], -backgroundOffset, 0)
+
+  -- render the graphics as per the state machine
+  gStateMachine:render()
+
+  push:finish()
+end
+
+
+
---
+++ 04 Match Three/Match Three 10/src/states/GameoverState.lua
@@ -0,0 +1,70 @@
+--[[
+  gameover state
+
+  showing:
+    - a heading
+    - the score
+    - how to proceed
+
+  allowing to:
+    - go to the start state
+]]
+
+-- inherit from the BaseState class
+GameoverState = Class{__includes = BaseState}
+
+-- in the enter() function
+function GameoverState:enter(params)
+  self.score = params.score
+end
+
+-- in the update(dt) function listen for a key press on a selection of keys
+function GameoverState:update(dt)
+  -- when pressing enter go to the start state
+  if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+    gStateMachine:change('start')
+    gSounds['select']:play()
+  end
+end
+
+-- in the render() function, display the gameover message atop the score achieved by the player and how to continue
+function GameoverState:render()
+  -- include the strings atop an overlay matching the play state
+  -- overlay centered in the page
+  love.graphics.setColor(0.1, 0.17, 0.35, 0.7)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - VIRTUAL_WIDTH / 6, VIRTUAL_HEIGHT / 2 - VIRTUAL_HEIGHT / 5, VIRTUAL_WIDTH / 3, VIRTUAL_HEIGHT / 2.5, 5)
+  love.graphics.setColor(0, 0, 0, 0.5)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - VIRTUAL_WIDTH / 6, VIRTUAL_HEIGHT / 2 - VIRTUAL_HEIGHT / 5, VIRTUAL_WIDTH / 3, VIRTUAL_HEIGHT / 2.5, 5)
+
+
+  -- include the heading and strings of text evenly space in the overlay
+  love.graphics.setFont(gFonts['big'])
+  love.graphics.setColor(0.42, 0.59, 0.94, 1)
+  love.graphics.printf(
+    'GAMEOVER',
+    0,
+    (VIRTUAL_HEIGHT / 2 - VIRTUAL_HEIGHT / 5) + (VIRTUAL_HEIGHT / 2.5) / 4 - 16,
+    VIRTUAL_WIDTH,
+    'center'
+  )
+
+  love.graphics.setFont(gFonts['normal'])
+
+  love.graphics.printf(
+    'Score: ' .. tostring(self.score),
+    0,
+    (VIRTUAL_HEIGHT / 2 - VIRTUAL_HEIGHT / 5) + (VIRTUAL_HEIGHT / 2.5) / 4 * 2 - 8,
+    VIRTUAL_WIDTH,
+    'center'
+  )
+
+  love.graphics.setFont(gFonts['small'])
+
+  love.graphics.printf(
+    'Press enter to continue',
+    0,
+    (VIRTUAL_HEIGHT / 2 - VIRTUAL_HEIGHT / 5) + (VIRTUAL_HEIGHT / 2.5) / 4 * 3 - 4,
+    VIRTUAL_WIDTH,
+    'center'
+  )
+end
