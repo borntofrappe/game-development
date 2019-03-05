ad62909c43634f30f9ff21fe10ce42f6e95f1bf2
gabrielecorti <borntofrappe@gmail.com>
2019-03-05 14:17:25 +0100

    add resources

---
+++ 05 Super Mario Bros/src/constants.lua
@@ -0,0 +1,12 @@
+-- include here those constant values used throughout the application
+
+-- measures for the screen width and height
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 576
+-- virtual measures for the push library
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- values for the tile map
+TILE_GROUND = 1
+TILE_SKY = 2
