a8ef1b1bb32414c23c47b46cb88b1fc29a8a5d56
gabrielecorti <borntofrappe@gmail.com>
2019-03-06 10:14:21 +0100

    describe and add the animation for the horizontal movement

--- 05 Super Mario Bros/Prep - Animation/README.md
+++ 05 Super Mario Bros/Prep - Animation/README.md
@@ -1,10 +1,12 @@
 # Animation
 
-While I have already made a first attempt in animating the character, by rapidly changing the stance through dedicated variables, the lecturer includes animation through a custom class, `Animation`.  I therefore decided to remove my variables in favor of this solution. This doesn't mean the code was include for nothing though. The logic of how the character is updated, like a flip book, and the way the character is made to look the opposite way, by setting the `xScale` to negative values, are concepts which repeat themselves.
+While I have already made a first attempt in animating the character, by rapidly changing the stance through dedicated variables, the lecturer includes animation through a custom class, `Animation`. I therefore decided to remove my variables in favor of this solution. This doesn't mean the code was include for nothing though. The logic of how the character is updated, like a flip book, and the way the character is made to look the opposite way, by setting the `xScale` to negative values, are concepts which repeat themselves.
 
-## Update 0 - animation.lua
+## Update 0
 
 I tried to comment the **Animation.lua** as much as possible, to understand the inner logic of the class, but here I'll also add a few notes jotted down while reviewing the video.
+
+### Animation.lua
 
 Here's the idea behind the class:
 
@@ -19,3 +21,84 @@ Back to the class:
 - using the table and set interval loop through the table and through a custom function, `getCurrentFrame`, return the frame referred to by the interval.
 
 The idea seems to have the class update the frames and use the function in `main.lua` to actually change the appearance of the character. Rather comprehensible once the `def` argument is understood.
+
+### moving.lua
+
+Once the `Animation` class is set up, its logic is incorporated in `main.lua` as follows (I use `moving.lua` as a temporary file to describe the update, but the logic is included in a `main.lua` file to have it run in the Love2D engine).
+
+- include the animation class in the list of dependencies of the game. Or simply include it atop the `lua` file;
+
+- create variables referring to the possible animation. The idea is to have a variable describe the current animation and update this variable following player input. Following the input of the player and selecting from a series of possible values.
+
+  In the current update, the possible animations are `idleAnimation` and `movingAnimation`, to have the character respectively stand still and move horizontally.
+
+  ```lua
+  idleAnimation = Animation {
+    frames = {1},
+    interval = 1
+  }
+  movingAnimation = Animation {
+    frames = {10, 11},
+    interval = 0.1
+  }
+  ```
+
+- initialize a variable referring, as mentioned, to the current animation, but also a variable referring to the current direction.
+
+  ```lua
+  currentAnimation = idleAnimation
+  currentDirection = 'right'
+  ```
+
+  This last one to have the character look and move to the left by scaling the quad to a negative `xScale` value (more on this soon).
+
+The code has thus included the animation class, but it is necessary to have it reflect its logic visually and update its logic following player input.
+
+To visually detail the change introduced by the class, and specifically in the `love.graphics.draw` function detailing the character:
+
+- include the current frame obtained through the `getCurrentFrame()` function in the argument detailing the quad.
+
+  ```lua
+  gFrames['character'][currentAnimation:getCurrentFrame()]
+  ```
+
+- include the `currentDirection` variable in the argument detailing the horizontal scale of the quad, to have it visually point to the left.
+
+  ```lua
+  currentDirection == 'right' and 1 or -1, -- x scale
+  gFrames['character'][currentAnimation:getCurrentFrame()]
+  ```
+
+- update the argument making up the `x` coordinate of the character in light of the changed `xScale` value. Indeed scaling back the value to `-1` causes the character to instantly teleport itself to the left by the amount of pixels making up its width. This is because the change in the scale is applied with the left edge of the character as a hinge, and the rotation effectively moves the character back a few pixels. To offset this force, add the width of the character, conditional to the direction being the one changing the `xScale` to a negative value.
+
+  ```lua
+  currentDirection == 'right' and math.floor(gCharacter.x) or math.floor(gCharacter.x + CHARACTER_WIDTH),
+  ```
+
+To change the animation and most importantly to have it update the appearance of the character, it is then necessary to update th logic in `love.update()`:
+
+- by default set the current animation to be the idle animation.
+
+  ```lua
+  currentAnimation = idleAnimation
+  ```
+
+  This to have the character always revert back to the idle state, and change the animation only following player input.
+
+- change the animation to refer to the `movingAnimation` when the arrow keys are pressed.
+
+  ```lua
+  currentAnimation = movingAnimation
+  ```
+
+  Change also the direction to have the character scaled to the right/left.
+
+  ```lua
+  currentDirection = 'right'
+  ```
+
+- do not forget to add the update logic through the `update(dt)` function.
+
+  ```lua
+  currentAnimation:update(dt)
+  ```
---
+++ 05 Super Mario Bros/Prep - Animation/moving.lua
@@ -0,0 +1,202 @@
+-- require Dependencies.lua, itself requiring every resource needed for the project
+require 'src/Dependencies'
+
+-- in the load function set up the game's title and screen
+function love.load()
+  love.window.setTitle('Animation')
+
+  -- GLOBALS
+  -- font
+  gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['normal'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['big'] = love.graphics.newFont('fonts/font.ttf', 24),
+    ['humongous'] = love.graphics.newFont('fonts/font.ttf', 56)
+  }
+
+  -- graphics
+  gTextures = {
+    ['tilesheet'] = love.graphics.newImage('graphics/tiles.png'),
+    ['backgrounds'] = love.graphics.newImage('graphics/backgrounds.png'),
+    ['character'] = love.graphics.newImage('graphics/character.png'),
+  }
+
+  -- assets created from the graphics
+  gFrames = {
+    ['tiles'] = GenerateQuads(gTextures['tilesheet'], TILE_SIZE, TILE_SIZE),
+    ['background'] = GenerateQuads(gTextures['backgrounds'], BACKGROUND_WIDTH, BACKGROUND_HEIGHT),
+    ['character'] = GenerateQuads(gTextures['character'], CHARACTER_WIDTH, CHARACTER_HEIGHT)
+  }
+
+  -- create a table in which to describe the tiles
+  tiles = {}
+  for y = 1, MAP_HEIGHT do
+    -- insert a table for each row
+    table.insert(tiles, {})
+    -- insert one table describing an id for each cell
+    for x = 1, MAP_WIDTH * 2 do
+      table.insert(tiles[y], {
+        -- include the ground tiles after as many rows as described by the MAP_SKY constant
+        id = y < MAP_SKY and TILE_SKY or TILE_GROUND
+      })
+    end
+  end
+
+  -- include a variable for the horizontal scroll of the camera
+  cameraScroll = 0
+
+  idleAnimation = Animation {
+    frames = {1},
+    interval = 1
+  }
+  movingAnimation = Animation {
+    frames = {10, 11},
+    interval = 0.1
+  }
+
+  -- include a variable to describe the animation and direction
+  -- this to have the character animated and pointing at the direction dictated through player input
+  currentAnimation = idleAnimation
+  currentDirection = 'right'
+
+  --[[
+  describe a table for the values needed to draw and move the character
+  - x and y describing where the character ought to be positioned
+  - stance to change the quad being drawn
+  - direction to allow for the character to move to the left
+  - offset to set the character in place when the direction is swapped
+  ]]
+  gCharacter = {
+    -- horizontally centered
+    ['x'] = (VIRTUAL_WIDTH / 4) - (CHARACTER_WIDTH / 2),
+    -- vertically right atop the ground tiles
+    ['y'] = (MAP_SKY - 1) * TILE_SIZE - CHARACTER_HEIGHT
+  }
+
+
+  -- PUSH
+  -- virtualization set up through the push library
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = true,
+    vsync = true
+  })
+
+  -- table recording the different keys being pressed
+  love.keyboard.keyPressed = {}
+end
+
+-- RESIZE
+-- in the resize function update the resolution through the push library
+function love.resize(width, height)
+  push:resize(width, height)
+end
+
+
+-- KEYPRESSES
+-- in the keypressed function keep track of the key being pressed in keyPressed
+function love.keypressed(key)
+  love.keyboard.keyPressed[key] = true
+end
+
+-- create a function to check whether a specific key was recorded in the keyPressed table
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keyPressed[key]
+end
+
+-- UPDATE
+function love.update(dt)
+  currentAnimation = idleAnimation
+
+  -- when the escape key is pressed quit the game early
+  if love.keyboard.wasPressed('escape') then
+    love.event.quit()
+  end
+
+  if love.keyboard.isDown('left') then
+    -- uodate the position of the character
+    -- ! clamp the coordinate to 0 to have the character stop when it reaches the left edge of the screen
+    gCharacter.x = math.max(0, gCharacter.x - CHARACTER_SCROLL_SPEED * dt)
+    currentDirection = 'left'
+    currentAnimation = movingAnimation
+  end
+
+  if love.keyboard.isDown('right') then
+  -- uodate the position of the character
+    gCharacter.x = gCharacter.x + CHARACTER_SCROLL_SPEED * dt
+    currentDirection = 'right'
+    currentAnimation = movingAnimation
+  end
+
+  -- update the animation
+  currentAnimation:update(dt)
+
+  -- update the position of the camera to have the character always perfectly centered on the screen
+  -- ! clamp the scroll to 0 to never show past the left edge of the screen
+  cameraScroll = math.max(0, gCharacter.x - (VIRTUAL_WIDTH / 2) + CHARACTER_WIDTH / 2)
+
+  -- reset keyPressed to an empty table
+  love.keyboard.keyPressed = {}
+end
+
+-- DRAW
+function love.draw()
+  push:start()
+
+  -- include one of the three possible backgrounds
+  love.graphics.draw(
+    gTextures['backgrounds'],
+    gFrames['background'][2],
+    0, -- x origin
+    0, -- y origin
+    0, -- rotation
+    VIRTUAL_WIDTH / BACKGROUND_WIDTH, -- x scale
+    VIRTUAL_HEIGHT / BACKGROUND_HEIGHT -- y scale
+  )
+
+
+  -- include a simple string in the top left corner
+  love.graphics.setFont(gFonts['big'])
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.print('Animation', 8, 8)
+
+
+  -- translate the camera using love.graphics.translate() and the cameraScroll variable
+  love.graphics.translate(-math.floor(cameraScroll), 0)
+
+  --[[
+    include the tiles looping through the tiles table
+  ]]
+  for y = 1, MAP_HEIGHT do
+    for x = 1, MAP_WIDTH * 2 do
+      local tile = tiles[y][x]
+
+      love.graphics.draw(
+        gTextures['tilesheet'],
+        gFrames['tiles'][tile.id],
+        (x - 1) * TILE_SIZE,
+        (y - 1) * TILE_SIZE
+      )
+
+    end
+  end
+
+
+  -- include the character atop the ground tiles
+  love.graphics.draw(
+    gTextures['character'],
+    gFrames['character'][currentAnimation:getCurrentFrame()],
+    -- use math.floor to avoid blur while moving the character
+    currentDirection == 'right' and math.floor(gCharacter.x) or math.floor(gCharacter.x + CHARACTER_WIDTH),
+    math.floor(gCharacter.y),
+    0, -- rotation
+    currentDirection == 'right' and 1 or -1, -- x scale
+    1 -- y scale
+  )
+
+  push:finish()
+end
+
+
+
