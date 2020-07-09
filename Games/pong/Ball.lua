ba8c7915c5862e0e41be6d8a7b664c2b4341f06e
borntofrappe <borntofrappe@gmail.com>
2020-07-09 23:59:44 +0200

    update paddles and ball

--- Games/pong/Ball.lua
+++ Games/pong/Ball.lua
@@ -7,9 +7,54 @@ function Ball:init(x, y, r)
     ball.y = y
     ball.r = r
 
+    ball.dx = math.random(-200, 200)
+    ball.dy = math.random(2) == 1 and 150 or -150
+
     self.__index = self
     setmetatable(ball, self)
     return ball
+end
+
+function Ball:update(dt)
+    self.x = self.x + dt * self.dx
+    self.y = self.y + dt * self.dy
+
+    if self.x < self.r then
+        self.x = self.r
+        self.dx = self.dx * -1
+    
+    elseif self.x > WINDOW_WIDTH - self.r then
+        self.x = WINDOW_WIDTH - self.r
+        self.dx = self.dx * -1
+    end
+
+end
+
+function Ball:collides(paddle)
+    x = paddle.x
+    y = paddle.y
+    r = paddle.r
+
+    return ((self.x - x) ^ 2 + (self.y - y) ^ 2) ^ 0.5 < r
+end
+
+function Ball:bounce(paddle)
+    self.dy = self.dy * -1.1
+
+    if self.x < paddle.x then
+        self.dx = -math.random(50, 200)
+    else
+        self.dx = math.random(50, 200)
+    end
+
+end
+
+function Ball:reset()
+    ball.x = WINDOW_WIDTH / 2
+    ball.y = WINDOW_HEIGHT / 2
+
+    ball.dx = math.random(-200, 200)
+    ball.dy = math.random(2) == 1 and 100 or -100
 end
 
 function Ball:render()
--- Games/pong/main.lua
+++ Games/pong/main.lua
@@ -9,15 +9,20 @@ SETTINGS = {
     vsync = true,
 }
 
+PADDLE_SPEED = 250
+
 function love.load()
     love.window.setTitle('Pong')
+    
+    math.randomseed(os.time())
+
     font = love.graphics.newFont('res/Righteous-Regular.ttf', 24)
     love.graphics.setFont(font)
 
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, SETTINGS)
 
-    player1 = Paddle:init(WINDOW_WIDTH / 2, 0, 30, false)
-    player2 = Paddle:init(WINDOW_WIDTH / 2, WINDOW_HEIGHT, 30, true)
+    player1 = Paddle:init(WINDOW_WIDTH / 2, WINDOW_HEIGHT, 30, true)
+    player2 = Paddle:init(WINDOW_WIDTH / 2, 0, 30, false)
 
     ball = Ball:init(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 8)
 end
@@ -29,11 +34,49 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    if love.keyboard.isDown('right') then
+        player1.dx = PADDLE_SPEED
+    elseif love.keyboard.isDown('left') then
+        player1.dx = -PADDLE_SPEED
+    else
+        player1.dx = 0
+    end
 
+    if love.keyboard.isDown('d') then
+        player2.dx = PADDLE_SPEED
+    elseif love.keyboard.isDown('a') then
+        player2.dx = -PADDLE_SPEED
+    else
+        player2.dx = 0
+    end
+
+
+    if ball:collides(player1) then
+        ball:bounce(player1)
+        
+    elseif ball:collides(player2) then
+        ball:bounce(player2)
+    end
+
+    if ball.y < 0 then
+        player1:score()
+        ball:reset()
+    elseif ball.y > WINDOW_HEIGHT then
+        player2:score()
+        ball:reset()
+    end
+
+    player1:update(dt)
+    player2:update(dt)
+
+    ball:update(dt)
+end
 
 function love.draw()
     love.graphics.clear(0, 0, 0, 1)
 
+    -- environment
     love.graphics.setColor(1, 1, 1, 0.05)
     love.graphics.arc('fill', WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 42, 0, math.pi) 
     love.graphics.setColor(1, 1, 1, 0.05)
@@ -43,12 +86,12 @@ function love.draw()
     love.graphics.line(0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT / 2) 
     love.graphics.circle('line', WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 42) 
     
-    --score at either side
+    --score
     love.graphics.translate(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
     love.graphics.rotate(math.pi)
-    love.graphics.printf(0, 0, 5, WINDOW_WIDTH / 2 - 8, 'right')
+    love.graphics.printf(player2.points, 0, 5, WINDOW_WIDTH / 2 - 8, 'right')
     love.graphics.rotate(math.pi)
-    love.graphics.printf(0, 0, 5, WINDOW_WIDTH / 2 - 8, 'right')
+    love.graphics.printf(player1.points, 0, 5, WINDOW_WIDTH / 2 - 8, 'right')
     love.graphics.translate(-WINDOW_WIDTH / 2, -WINDOW_HEIGHT / 2)
     
     -- ball
