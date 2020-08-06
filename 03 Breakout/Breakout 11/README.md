c19834082e4b05f287543f3dabe05d1ca1065c73
borntofrappe <borntofrappe@gmail.com>
2020-08-06 22:57:52 +0200

    add and show high scores

---
+++ 03 Breakout/Breakout 11/README.md
@@ -0,0 +1,81 @@
+Include high scores in a local file and in a separate game state.
+
+## Data
+
+The idea is to store the data in a `lst` file, dedicating two lines for each high score.
+
+```lst
+name
+score
+name
+score
+name
+score
+```
+
+One line for the player name, one for actual score.
+
+When reading the data, the idea is to then produce a table, where each pairing occupies a key.
+
+```lua
+highscores = {
+  [1] = {
+    name,
+    score
+  },
+  [2] = {
+    name,
+    score
+  }
+}
+```
+
+## loadHighScores
+
+The relevant interface is the one provided by [love.filesystem](https://love2d.org/wiki/love.filesystem). `loadHighScores` makes use of several functions from this module, in order to:
+
+1. set the current folder
+
+   ```lua
+   love.filesystem.setIdentity('breakout')
+   ```
+
+2. check if a file already exist
+
+   ```lua
+   love.filesystem.getInfo('highscores.lst')
+   ```
+
+3. create arbitrary data in case the file doesn't exist
+
+   ```lua
+   love.filesystem.write('highscores.lst', highscores)
+   ```
+
+4. read from the sure-to-exist file one line at a time
+
+   ```lua
+   for line in love.filesystem.lines('highscores.lst') do
+     -- do something
+   end
+   ```
+
+## HighScoresState
+
+In terms of flow, you access the high scores from the start state, when the variable `choice` describes the matching option. By pressing escape or enter you then move back to the start state.
+
+In terms of content, the state loads the high scores as through the `loadHighScores` function, and then iterates through the results to render the name and scores one after the other.
+
+```lua
+function HighScoresState:init()
+  self.highScores = loadHighScores()
+end
+
+function HighScoresState:render()
+  for k, highScore in pairs(self.highScores) do
+    -- render name and score
+  end
+end
+```
+
+The most challenging part is figuring out the space and alignment of the text.
---
+++ 03 Breakout/Breakout 11/src/states/ServeState.lua
@@ -0,0 +1,81 @@
+ServeState = Class({__includes = BaseState})
+
+function ServeState:init()
+  self.level = 1
+  self.health = 3
+  self.maxHealth = 3
+  self.score = 0
+  
+  self.paddle = Paddle(VIRTUAL_WIDTH / 2 - 32, VIRTUAL_HEIGHT - 32)
+  self.ball = Ball(VIRTUAL_WIDTH / 2 - 4, VIRTUAL_HEIGHT - 32 - 8)
+  self.bricks = LevelMaker.createMap(self.level)
+end
+
+function ServeState:enter(params)
+  if params then
+    self.level = params.level
+    self.health = params.health
+    self.maxHealth = params.maxHealth
+    self.score = params.score
+
+    self.paddle = params.paddle
+    self.bricks = params.bricks or LevelMaker.createMap(self.level)
+
+    self.ball.x = self.paddle.x + self.paddle.width / 2 - 4
+    self.ball.y = self.paddle.y - self.ball.height
+  end
+end
+
+function ServeState:update(dt)
+  if love.keyboard.waspressed('escape') then
+    gStateMachine:change('start')
+    gSounds['confirm']:play()
+  end
+
+  if love.keyboard.waspressed('enter') or love.keyboard.waspressed('return') then
+    gStateMachine:change('play', {
+      level = self.level,
+      health = self.health,
+      maxHealth = self.maxHealth,
+      score = self.score,
+      paddle = self.paddle,
+      ball = self.ball,
+      bricks = self.bricks
+    })
+    gSounds['confirm']:play()
+  end
+
+  self.paddle:update(dt)
+  self.ball.x = self.paddle.x + self.paddle.width / 2 - 4
+end
+
+function ServeState:render()
+  displayHealth(self.health, self.maxHealth)
+  displayScore(self.score)
+
+  for k, brick in pairs(self.bricks) do
+    brick:render()  
+  end
+  self.paddle:render()
+  self.ball:render()
+
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.setFont(gFonts['big'])
+  love.graphics.printf(
+    'Level: ' .. self.level,
+    0,
+    VIRTUAL_HEIGHT / 2 - 20,
+    VIRTUAL_WIDTH,
+    'center'
+  )
+
+  love.graphics.setFont(gFonts['normal'])
+  love.graphics.printf(
+    'Press enter to play',
+    0,
+    VIRTUAL_HEIGHT / 2 + 16,
+    VIRTUAL_WIDTH,
+    'center'
+  )
+
+end
