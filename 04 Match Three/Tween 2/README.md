8f36e09e808d55a8251daed62a0d2ff429ffaa12
borntofrappe <borntofrappe@gmail.com>
2020-08-11 11:35:45 +0200

    tween with timer library

---
+++ 04 Match Three/Tween 2/README.md
@@ -0,0 +1,72 @@
+The approach described in _Tween 0_ and _Tween 1_ works, but becomes brittle the more values you need to change over time.
+
+With this script, the goal is to change the position, but also the opacity of the images. It serves to illustrate the usefulness of the **knife** library and the `Timer` module.
+
+## Opacity
+
+You change the opacity with the fourth argument of the `setColor` function.
+
+```lua
+love.graphics.setColor(1, 1, 1, alpha)
+love.graphics.draw(BIRD_IMAGE, bird.x, bird.y)
+```
+
+Without the module mentioned above, you'd modify the opacity with an additional variable.
+
+```lua
+function love.load()
+    for k, bird in pairs(birds) do
+      -- previous attributes
+      bird.opacity = 0
+    end
+end
+
+function love.update(dt)
+  if timer < TIMER_MAX then
+    timer = timer + dt
+    for k, bird in pairs(birds) do
+      -- update bird.x
+      bird.opacity = math.min(1, bird.opacity + 1 / bird.rate * dt)
+    end
+  end
+end
+
+function love.draw()
+  for k, bird in pairs(birds) do
+    love.graphics.setColor(1, 1, 1, bird.opacity)
+    love.graphics.draw(BIRD_IMAGE, bird.x, bird.y)
+  end
+end
+```
+
+See the code in `min.lua` for reference.
+
+## Timer
+
+The relevant function is here `Timer.tween`. It accepts as argument a rate, and a table defining the tween's own rules.
+
+For instance and to modify the horizontal coordinate.
+
+```lua
+Timer.tween(bird.rate, {
+  [bird] = { x = VIRTUAL_WIDTH - BIRD_WIDTH }
+})
+```
+
+This has the effect of modifying the `x` attribute of the `bird` entity.
+
+To modify the opacity as well, all you need is another variable-value pair in the table describing which values to change.
+
+```lua
+Timer.tween(bird.rate, {
+  [bird] = { x = VIRTUAL_WIDTH - BIRD_WIDTH, opacity = 1 }
+})
+```
+
+In both instances, the `update` function needs to update the `Timer` object only.
+
+```lua
+function love.update(dt)
+  Timer.update(dt)
+end
+```
---
+++ 04 Match Three/Tween 2/main.lua
@@ -0,0 +1,100 @@
+push = require "res/lib/push"
+Timer = require "res/lib/knife/timer"
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 384
+VIRTUAL_HEIGHT = 216
+
+OPTIONS = {
+  fullscreen = false,
+  vsync = true,
+  resizable = true
+}
+
+BIRD_IMAGE = love.graphics.newImage("bird.png")
+BIRD_WIDTH = BIRD_IMAGE:getWidth()
+BIRD_HEIGHT = BIRD_IMAGE:getHeight()
+
+TIMER_MAX = 10
+
+function love.load()
+  math.randomseed(os.time())
+
+  timer = 0
+
+  birds = {}
+  for i = 1, 100 do
+    table.insert(
+      birds,
+      {
+        x = 0,
+        y = math.random(0, VIRTUAL_HEIGHT - BIRD_HEIGHT),
+        opacity = 0,
+        rate = math.random() * TIMER_MAX
+      }
+    )
+  end
+
+  for k, bird in pairs(birds) do
+    Timer.tween(
+      bird.rate,
+      {
+        [bird] = {x = VIRTUAL_WIDTH - BIRD_WIDTH, opacity = 1}
+      }
+    )
+  end
+
+  love.window.setTitle("Tween")
+  font = love.graphics.newFont("res/fonts/font.ttf", 24)
+  love.graphics.setFont(font)
+
+  love.graphics.setDefaultFilter("nearest", "nearest")
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, OPTIONS)
+end
+
+function love.resize(width, height)
+  push:resize(width, height)
+end
+
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.quit()
+  end
+
+  if key == "r" then
+    timer = 0
+    for k, bird in pairs(birds) do
+      bird.x = 0
+      bird.y = math.random(0, VIRTUAL_HEIGHT - BIRD_HEIGHT)
+      bird.opacity = 0
+      bird.rate = math.random() * TIMER_MAX
+      Timer.tween(
+        bird.rate,
+        {
+          [bird] = {x = VIRTUAL_WIDTH - BIRD_WIDTH, opacity = 1}
+        }
+      )
+    end
+  end
+end
+
+function love.update(dt)
+  if timer < TIMER_MAX then
+    timer = timer + dt
+    Timer.update(dt)
+  end
+end
+
+function love.draw()
+  push:start()
+
+  love.graphics.print(timer, 8, 8)
+  for k, bird in pairs(birds) do
+    love.graphics.setColor(1, 1, 1, bird.opacity)
+    love.graphics.draw(BIRD_IMAGE, bird.x, bird.y)
+  end
+
+  push:finish()
+end
