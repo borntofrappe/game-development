86f0fe32edadaa8d10eddffd55b90fbd462f1a7a
borntofrappe <borntofrappe@gmail.com>
2020-10-24 10:45:44 +0200

    structure project folder

--- Games/Tetris/main.lua
+++ Games/Tetris/main.lua
@@ -1,76 +1,56 @@
-TILE_SIZE = 24
-COLUMNS = 10
-ROWS = 15
-PADDING = {
-  ["top"] = 64,
-  ["left"] = 64,
-  ["right"] = 64,
-  ["bottom"] = 32
-}
-
-WINDOW_WIDTH = TILE_SIZE * COLUMNS + PADDING.left + PADDING.right
-WINDOW_HEIGHT = TILE_SIZE * ROWS + PADDING.top + PADDING.bottom
-
-function GenerateQuads(atlas)
-  local quads = {}
-  for x = 1, math.floor(atlas:getWidth() / TILE_SIZE) do
-    for y = 1, math.floor(atlas:getHeight() / TILE_SIZE) do
-      table.insert(
-        quads,
-        love.graphics.newQuad((x - 1) * TILE_SIZE, (y - 1) * TILE_SIZE, TILE_SIZE, TILE_SIZE, atlas:getDimensions())
-      )
-    end
-  end
-
-  return quads
-end
+require "src/Dependencies"
 
 function love.load()
   love.window.setTitle("Tetris")
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
-  love.graphics.setBackgroundColor(0.1, 0.15, 0.18)
+  love.graphics.setBackgroundColor(0.07, 0.1, 0.12)
 
   grid = {
     ["columns"] = COLUMNS,
     ["rows"] = ROWS,
     ["cellSize"] = TILE_SIZE
   }
+  showGrid = false
 
-  score = 0
-
-  gFonts = {
-    ["big"] = love.graphics.newFont("res/font.ttf", 36),
-    ["normal"] = love.graphics.newFont("res/font.ttf", 20),
-    ["small"] = love.graphics.newFont("res/font.ttf", 14)
-  }
-
-  tilesheet = love.graphics.newImage("res/tilesheet.png")
-  quads = GenerateQuads(tilesheet)
+  love.keyboard.keyPressed = {}
 end
 
 function love.keypressed(key)
-  if key == "escape" then
-    love.event.quit()
-  end
+  love.keyboard.keyPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keyPressed[key]
 end
 
 function love.update(dt)
+  if love.keyboard.wasPressed("escape") then
+    love.event.quit()
+  end
+  if love.keyboard.wasPressed("g") then
+    showGrid = not showGrid
+  end
+
+  love.keyboard.keyPressed = {}
 end
 
 function love.draw()
-  love.graphics.translate(PADDING.left, PADDING.top)
-  love.graphics.setLineWidth(2)
-  love.graphics.setColor(0.9, 0.9, 0.9)
-  for x = 1, grid.columns + 1 do
-    love.graphics.line((x - 1) * grid.cellSize, 0, (x - 1) * grid.cellSize, grid.rows * grid.cellSize)
-  end
-  for y = 1, grid.rows + 1 do
-    love.graphics.line(0, (y - 1) * grid.cellSize, grid.columns * grid.cellSize, (y - 1) * grid.cellSize)
+  love.graphics.translate(TILE_SIZE, 0)
+
+  if showGrid then
+    love.graphics.setLineWidth(1)
+    love.graphics.setColor(0.9, 0.9, 0.9)
+    for x = 1, grid.columns + 1 do
+      love.graphics.line((x - 1) * grid.cellSize, 0, (x - 1) * grid.cellSize, grid.rows * grid.cellSize)
+    end
+    for y = 1, grid.rows + 1 do
+      love.graphics.line(0, (y - 1) * grid.cellSize, grid.columns * grid.cellSize, (y - 1) * grid.cellSize)
+    end
   end
 
   love.graphics.setColor(1, 1, 1)
-  love.graphics.draw(tilesheet, quads[1], 0, 0)
-  love.graphics.draw(tilesheet, quads[2], TILE_SIZE, 0)
-  love.graphics.draw(tilesheet, quads[3], TILE_SIZE * 2, TILE_SIZE)
-  love.graphics.draw(tilesheet, quads[4], TILE_SIZE * 3, 0)
+  love.graphics.draw(gTextures["tiles"], gFrames["tiles"][2], 0, 0)
+  love.graphics.draw(gTextures["tiles"], gFrames["tiles"][2], TILE_SIZE, 0)
+  love.graphics.draw(gTextures["tiles"], gFrames["tiles"][2], TILE_SIZE * 2, 0)
+  love.graphics.draw(gTextures["tiles"], gFrames["tiles"][2], TILE_SIZE, TILE_SIZE)
 end
---
+++ Games/Tetris/src/Dependencies.lua
@@ -0,0 +1,16 @@
+require "src/constants"
+require "src/Utils"
+
+gTextures = {
+  ["tiles"] = love.graphics.newImage("res/tilesheet.png")
+}
+
+gFrames = {
+  ["tiles"] = GenerateQuads(gTextures.tiles, TILE_SIZE, TILE_SIZE)
+}
+
+gFonts = {
+  ["big"] = love.graphics.newFont("res/font.ttf", 36),
+  ["normal"] = love.graphics.newFont("res/font.ttf", 20),
+  ["small"] = love.graphics.newFont("res/font.ttf", 14)
+}
