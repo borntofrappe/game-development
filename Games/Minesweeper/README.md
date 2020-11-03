43f24d9defe5be07bf44daa9d70b8de0e89de7f9
borntofrappe <borntofrappe@gmail.com>
2020-11-03 10:08:40 +0100

    [style] introduce colors and UI

--- Games/Minesweeper/README.md
+++ Games/Minesweeper/README.md
@@ -7,3 +7,19 @@ The goal is to create the game minesweeper with Lua and Love2D. This works more 
 [A coding challenge](https://thecodingtrain.com/CodingChallenges/071-minesweeper.html) from the [coding train website](https://thecodingtrain.com/) works to explain the game in the context of Processing.
 
 The built-in version available from the "Google Play Games" app works as an inspiration for the design and graphics.
+
+## Features
+
+The game is actively in development, and the following works more as a wishlist more than a collection of available features.
+
+- mouse and keyboard input
+
+- easy, medium, hard mode, changing the number of mines and the size of the grid as detailed in the following table
+
+  | Mode   | Mines | Columns | Rows |
+  | ------ | ----- | ------- | ---- |
+  | Easy   | 10    | 7       | 11   |
+  | Medium | 35    | 11      | 17   |
+  | Hard   | 75    | 15      | 23   |
+
+- optional audio
--- Games/Minesweeper/main.lua
+++ Games/Minesweeper/main.lua
@@ -1,1 +1,75 @@
--- minesweeper
+require "src/Dependencies"
+
+function love.load()
+  love.window.setTitle("Minesweeper")
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+  love.graphics.setBackgroundColor(
+    gColors["background-light"].r,
+    gColors["background-light"].g,
+    gColors["background-light"].b
+  )
+
+  love.graphics.setFont(gFonts["normal"])
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+  love.graphics.setColor(1, 1, 1)
+  for x = 1, COLUMNS do
+    for y = 1, ROWS do
+      local color = (x + y) % 2 == 0 and "cell-light" or "cell-dark"
+      love.graphics.setColor(gColors[color].r, gColors[color].g, gColors[color].b)
+      love.graphics.rectangle(
+        "fill",
+        PADDING + (x - 1) * CELL_SIZE,
+        PADDING + (y - 1) * CELL_SIZE,
+        CELL_SIZE,
+        CELL_SIZE
+      )
+    end
+  end
+
+  love.graphics.setColor(gColors["reveal-light"].r, gColors["reveal-light"].g, gColors["reveal-light"].b)
+  love.graphics.rectangle("fill", PADDING + (4) * CELL_SIZE, PADDING + (4) * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+  love.graphics.setColor(gColors["reveal-dark"].r, gColors["reveal-dark"].g, gColors["reveal-dark"].b)
+  love.graphics.rectangle("fill", PADDING + (5) * CELL_SIZE, PADDING + (4) * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+  love.graphics.setColor(gColors["reveal-light"].r, gColors["reveal-light"].g, gColors["reveal-light"].b)
+  love.graphics.rectangle("fill", PADDING + (6) * CELL_SIZE, PADDING + (4) * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+
+  love.graphics.setColor(gColors["number-1"].r, gColors["number-1"].g, gColors["number-1"].b)
+  love.graphics.printf(
+    1,
+    PADDING + (4) * CELL_SIZE,
+    PADDING + (4) * CELL_SIZE + CELL_SIZE / 2 - gFonts["normal"]:getHeight() / 2,
+    CELL_SIZE,
+    "center"
+  )
+  love.graphics.setColor(gColors["number-2"].r, gColors["number-2"].g, gColors["number-2"].b)
+  love.graphics.printf(
+    2,
+    PADDING + (5) * CELL_SIZE,
+    PADDING + (4) * CELL_SIZE + CELL_SIZE / 2 - gFonts["normal"]:getHeight() / 2,
+    CELL_SIZE,
+    "center"
+  )
+  love.graphics.setColor(gColors["number-3"].r, gColors["number-3"].g, gColors["number-3"].b)
+  love.graphics.printf(
+    3,
+    PADDING + (6) * CELL_SIZE,
+    PADDING + (4) * CELL_SIZE + CELL_SIZE / 2 - gFonts["normal"]:getHeight() / 2,
+    CELL_SIZE,
+    "center"
+  )
+
+  love.graphics.setColor(gColors["mine"].r, gColors["mine"].g, gColors["mine"].b)
+  love.graphics.rectangle("fill", PADDING + (3) * CELL_SIZE, PADDING + (7) * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+  love.graphics.setColor(0, 0, 0, 0.25)
+  love.graphics.circle(
+    "fill",
+    PADDING + (3) * CELL_SIZE + CELL_SIZE / 2,
+    PADDING + (7) * CELL_SIZE + CELL_SIZE / 2,
+    CELL_SIZE / 5
+  )
+end
