6fa8e3cbaf3dcd089582aceaeefd1341e6a55806
borntofrappe <borntofrappe@gmail.com>
2020-11-14 00:09:43 +0100

    [feature] add box2d logic to main game

--- Games/Lunar Lander/main.lua
+++ Games/Lunar Lander/main.lua
@@ -17,7 +17,40 @@ function love.load()
     ["vertical speed"] = 0
   }
 
-  terrain = makeTerrain()
+  love.physics.setMeter(METER)
+  world = love.physics.newWorld(0, GRAVITY * METER, true)
+
+  lander = {}
+  lander.body = love.physics.newBody(world, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, "dynamic")
+  lander.core = {}
+  lander.core.shape = love.physics.newCircleShape(10)
+  lander.core.fixture = love.physics.newFixture(lander.body, lander.core.shape)
+
+  lander.landingGear = {}
+  lander.landingGear[1] = {}
+  lander.landingGear[1].shape = love.physics.newPolygonShape(-11, 13, -8, 9, -6, 9, -3, 13)
+  lander.landingGear[1].fixture = love.physics.newFixture(lander.body, lander.landingGear[1].shape)
+
+  lander.landingGear[2] = {}
+  lander.landingGear[2].shape = love.physics.newPolygonShape(11, 13, 8, 9, 6, 9, 3, 13)
+  lander.landingGear[2].fixture = love.physics.newFixture(lander.body, lander.landingGear[2].shape)
+
+  lander.landingGear[3] = {}
+  lander.landingGear[3].shape = love.physics.newRectangleShape(0, 8.5, 16, 2)
+  lander.landingGear[3].fixture = love.physics.newFixture(lander.body, lander.landingGear[3].shape)
+  lander.landingGear[3].fixture:setSensor(true)
+
+  terrain = {}
+  terrain.body = love.physics.newBody(world, 0, WINDOW_HEIGHT)
+
+  local terrainPoints = getTerrainPoints()
+  table.insert(terrainPoints, WINDOW_WIDTH)
+  table.insert(terrainPoints, 0)
+  table.insert(terrainPoints, 0)
+  table.insert(terrainPoints, 0)
+
+  terrain.shape = love.physics.newChainShape(false, terrainPoints)
+  terrain.fixture = love.physics.newFixture(terrain.body, terrain.shape)
 
   love.keyboard.keyPressed = {}
 end
@@ -27,22 +60,101 @@ function love.keypressed(key)
     love.event.quit()
   end
 
-  love.keyboard.keyPressed[key] = true
-end
+  if key:lower() == "r" then
+    lander.body:setPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+    lander.body:setLinearVelocity(0, 0)
+    lander.body:setAngularVelocity(0)
+    lander.body:setAngle(0)
+  end
 
-function love.keyboard.wasPressed(key)
-  return love.keyboard.keyPressed[key]
+  if key == "up" then
+    lander.body:applyLinearImpulse(0, -IMPULSE)
+  end
+  if key == "right" then
+    lander.body:applyLinearImpulse(math.floor(IMPULSE / 2), 0)
+  elseif key == "left" then
+    lander.body:applyLinearImpulse(-math.floor(IMPULSE / 2), 0)
+  end
 end
 
 function love.update(dt)
-  love.keyboard.keyPressed = {}
+  world:update(dt)
+
+  local vx, vy = lander.body:getLinearVelocity()
+  data["horizontal speed"] = math.floor(vx)
+  data["vertical speed"] = math.floor(vy)
+
+  if love.keyboard.isDown("up") then
+    lander.body:applyForce(0, -VELOCITY)
+    data["fuel"] = data["fuel"] - 1
+  end
+  if love.keyboard.isDown("right") then
+    data["fuel"] = data["fuel"] - 0.5
+    lander.body:applyForce(VELOCITY, 0)
+  elseif love.keyboard.isDown("left") then
+    data["fuel"] = data["fuel"] - 0.5
+    lander.body:applyForce(-VELOCITY, 0)
+  end
 end
 
 function love.draw()
   love.graphics.setColor(0.85, 0.85, 0.85)
-  love.graphics.line(terrain)
+  love.graphics.setLineWidth(1)
+  love.graphics.polygon("line", terrain.body:getWorldPoints(terrain.shape:getPoints()))
 
-  displayData(
+  love.graphics.setLineWidth(2)
+  love.graphics.circle("line", lander.body:getX(), lander.body:getY(), lander.core.shape:getRadius())
+  for i, gear in ipairs(lander.landingGear) do
+    love.graphics.polygon("line", lander.body:getWorldPoints(gear.shape:getPoints()))
+  end
+
+  if love.keyboard.isDown("up") then
+    love.graphics.polygon(
+      "line",
+      lander.body:getX() - 9,
+      lander.body:getY() + 13,
+      lander.body:getX() - 7,
+      lander.body:getY() + 16,
+      lander.body:getX() - 5,
+      lander.body:getY() + 13
+    )
+    love.graphics.polygon(
+      "line",
+      lander.body:getX() + 9,
+      lander.body:getY() + 13,
+      lander.body:getX() + 7,
+      lander.body:getY() + 16,
+      lander.body:getX() + 5,
+      lander.body:getY() + 13
+    )
+  end
+  if love.keyboard.isDown("right") then
+    love.graphics.polygon(
+      "line",
+      lander.body:getX() - 8,
+      lander.body:getY() + 5.5,
+      lander.body:getX() - 12.5,
+      lander.body:getY() + 8,
+      lander.body:getX() - 8,
+      lander.body:getY() + 10.5
+    )
+  elseif love.keyboard.isDown("left") then
+    love.graphics.polygon(
+      "line",
+      lander.body:getX() + 8,
+      lander.body:getY() + 5.5,
+      lander.body:getX() + 12.5,
+      lander.body:getY() + 8,
+      lander.body:getX() + 8,
+      lander.body:getY() + 10.5
+    )
+  end
+
+  displayData()
+end
+
+function displayData()
+  displayKeyValuePairs(
     {"score", "time", "fuel"},
     {formatNumber, formatTime, formatNumber},
     WINDOW_WIDTH / 6 + 8,
@@ -51,7 +163,7 @@ function love.draw()
     font:getHeight() * 0.9
   )
 
-  displayData(
+  displayKeyValuePairs(
     {"altitude", "horizontal speed", "vertical speed"},
     {nil, formatHorizontalSpeed, formatVerticalSpeed},
     WINDOW_WIDTH / 2 + 16,
@@ -61,7 +173,7 @@ function love.draw()
   )
 end
 
-function displayData(keys, formattingFunctions, startX, startY, gapX, gapY)
+function displayKeyValuePairs(keys, formattingFunctions, startX, startY, gapX, gapY)
   for i, key in ipairs(keys) do
     local formattingFunction = formattingFunctions[i]
     local value = formattingFunction and formattingFunction(data[key]) or data[key]
--- Games/Lunar Lander/src/Utils.lua
+++ Games/Lunar Lander/src/Utils.lua
@@ -1,22 +1,22 @@
-function makeTerrain()
-  local terrain = {}
+function getTerrainPoints()
+  local points = {}
   local offset = love.math.random(1000)
   local increment = 0.1
-  local points = 50
+  local numberPoints = 50
   local terrainHeightRandom = love.math.random(15, 30)
   local terrainHeightNoise = WINDOW_HEIGHT / 2 - terrainHeightRandom
 
-  for i = 0, points do
-    local x = i * WINDOW_WIDTH / points
-    local y = WINDOW_HEIGHT - (love.math.noise(offset) * terrainHeightNoise + love.math.random() * terrainHeightRandom)
+  for i = 0, numberPoints do
+    local x = i * WINDOW_WIDTH / numberPoints
+    local y = (love.math.noise(offset) * terrainHeightNoise + love.math.random() * terrainHeightRandom) * -1
 
-    table.insert(terrain, x)
-    table.insert(terrain, y)
+    table.insert(points, x)
+    table.insert(points, y)
 
     offset = offset + increment
   end
 
-  return terrain
+  return points
 end
 
 function formatNumber(number, padding)
