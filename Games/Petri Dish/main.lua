8f6fcb87fad498fadb8f6c7705d67c681f56f497
borntofrappe <borntofrappe@gmail.com>
2020-11-21 10:23:22 +0100

    [game] translate viewpoint

--- Games/Petri Dish/main.lua
+++ Games/Petri Dish/main.lua
@@ -5,6 +5,10 @@ function love.load()
   love.window.setTitle("Petri Dish")
   love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
 
+  translation = {
+    ["x"] = 0,
+    ["y"] = 0
+  }
   player = Player:new()
   particles = {}
   for i = 0, PARTICLES do
@@ -24,11 +28,13 @@ function love.update(dt)
   Timer:update(dt)
   if love.mouse.isDown(1) then
     local x, y = love.mouse:getPosition()
+    local dx = player.x - x
+    local dy = player.y - y
     Timer:after(
       0.1,
       function()
         for i, tween in ipairs(Timer.tweens) do
-          if tween.label == "player" then
+          if tween.label == "translation" then
             table.remove(Timer.tweens, i)
           end
         end
@@ -36,9 +42,9 @@ function love.update(dt)
         Timer:tween(
           0.5,
           {
-            [player] = {["x"] = x, ["y"] = y}
+            [translation] = {["x"] = translation.x + dx, ["y"] = translation.y + dy}
           },
-          "player"
+          "translation"
         )
       end
     )
@@ -46,8 +52,9 @@ function love.update(dt)
 end
 
 function love.draw()
+  player:render()
+  love.graphics.translate(translation.x, translation.y)
   for i, particle in ipairs(particles) do
     particle:render()
   end
-  player:render()
 end
