fd993052ab9a3ae33998998bc2bec9b0876f101b
borntofrappe <borntofrappe@gmail.com>
2020-11-28 16:55:12 +0100

    add world constraints

--- Games/Surround/main.lua
+++ Games/Surround/main.lua
@@ -3,15 +3,26 @@ require "src/Dependencies"
 function love.load()
   love.window.setTitle("Surround")
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
-  love.graphics.setBackgroundColor(1, 1, 1)
+  love.graphics.setBackgroundColor(0.3, 0.3, 0.3)
 
   love.keyboard.keyPressed = {}
 
-  player = Player:new(math.floor(WINDOW_WIDTH / 2) / CELL_SIZE, math.floor(WINDOW_HEIGHT / 2) / CELL_SIZE)
-  translate = {
-    ["column"] = 0,
-    ["row"] = 0
+  world = {
+    ["columns"] = COLUMNS * 2,
+    ["rows"] = ROWS * 2,
+    ["translate"] = {
+      ["columns"] = COLUMNS,
+      ["rows"] = ROWS
+    }
   }
+
+  player = Player:new(love.math.random(world.columns), love.math.random(world.rows))
+
+  translate = {
+    ["column"] = -(player.column - math.floor(world.translate.columns / 2)),
+    ["row"] = -(player.row - math.floor(world.translate.rows / 2))
+  }
+
   updateWorld()
 end
 
@@ -30,11 +41,14 @@ end
 function love.update(dt)
   Timer:update(dt)
   player:update(dt)
+
   love.keyboard.keyPressed = {}
 end
 
 function love.draw()
   love.graphics.translate((translate.column) * CELL_SIZE, (translate.row) * CELL_SIZE)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", 0, 0, world.columns * CELL_SIZE, world.rows * CELL_SIZE)
   player:render()
 end
 
@@ -69,6 +83,15 @@ function updateWorld()
       player.row = player.row + player.d.r
       translate.column = translate.column - player.d.c
       translate.row = translate.row - player.d.r
+
+      if player.column < 1 or player.column > world.columns or player.row < 1 or player.row > world.rows then
+        player = Player:new(love.math.random(world.columns), love.math.random(world.rows))
+
+        translate = {
+          ["column"] = -(player.column - math.floor(world.translate.columns / 2)),
+          ["row"] = -(player.row - math.floor(world.translate.rows / 2))
+        }
+      end
     end
   )
 end
