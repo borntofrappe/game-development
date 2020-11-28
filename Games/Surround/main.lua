f4f3ecd681aac04f525539f7f61cad814ed38dad
borntofrappe <borntofrappe@gmail.com>
2020-11-28 14:52:48 +0100

    translate game and center player

--- Games/Surround/main.lua
+++ Games/Surround/main.lua
@@ -7,8 +7,12 @@ function love.load()
 
   love.keyboard.keyPressed = {}
 
-  player = Player:new(1, 1)
-  player:move()
+  player = Player:new(math.floor(WINDOW_WIDTH / 2) / CELL_SIZE, math.floor(WINDOW_HEIGHT / 2) / CELL_SIZE)
+  translate = {
+    ["column"] = 0,
+    ["row"] = 0
+  }
+  updateWorld()
 end
 
 function love.keypressed(key)
@@ -24,10 +28,47 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
+  Timer:update(dt)
   player:update(dt)
   love.keyboard.keyPressed = {}
 end
 
 function love.draw()
+  love.graphics.translate((translate.column) * CELL_SIZE, (translate.row) * CELL_SIZE)
   player:render()
+end
+
+function updateWorld()
+  Timer:every(
+    INTERVAL,
+    function()
+      if player.d.c ~= 0 or player.d.r ~= 0 then
+        local hasTrail = false
+        local previousTail = {}
+        for i, tail in ipairs(player.trail) do
+          if tail.column == player.column and tail.row == player.row then
+            hasTrail = true
+            previousTail = tail
+            break
+          end
+        end
+
+        if not hasTrail then
+          table.insert(
+            player.trail,
+            {
+              ["column"] = player.column,
+              ["row"] = player.row
+            }
+          )
+        else
+        end
+      end
+
+      player.column = player.column + player.d.c
+      player.row = player.row + player.d.r
+      translate.column = translate.column - player.d.c
+      translate.row = translate.row - player.d.r
+    end
+  )
 end
--- Games/Surround/src/Player.lua
+++ Games/Surround/src/Player.lua
@@ -12,9 +12,9 @@ function Player:new(column, row)
     },
     ["trail"] = {},
     ["color"] = {
-      ["r"] = 0.62, -- 0.16
-      ["g"] = 0, -- 0.83
-      ["b"] = 1 -- 0.69
+      ["r"] = 0.16, -- [0.62, 0.16]
+      ["g"] = 0.83, -- [0, 0.83]
+      ["b"] = 0.69 -- [1, 0.69]
     }
   }
 
@@ -24,7 +24,6 @@ function Player:new(column, row)
 end
 
 function Player:update(dt)
-  Timer:update(dt)
   if love.keyboard.wasPressed("up") then
     self.d.c = 0
     self.d.r = -1
@@ -52,37 +51,4 @@ function Player:render()
 
   love.graphics.setColor(self.color.r, self.color.g, self.color.b)
   love.graphics.rectangle("fill", (self.column - 1) * self.size, (self.row - 1) * self.size, self.size, self.size)
-end
-
-function Player:move()
-  Timer:every(
-    INTERVAL,
-    function()
-      if self.d.c ~= 0 or self.d.r ~= 0 then
-        local hasTrail = false
-        local previousTail = {}
-        for i, tail in ipairs(self.trail) do
-          if tail.column == self.column and tail.row == self.row then
-            hasTrail = true
-            previousTail = tail
-            break
-          end
-        end
-
-        if not hasTrail then
-          table.insert(
-            self.trail,
-            {
-              ["column"] = self.column,
-              ["row"] = self.row
-            }
-          )
-        else
-        end
-      end
-
-      self.column = self.column + self.d.c
-      self.row = self.row + self.d.r
-    end
-  )
 end
