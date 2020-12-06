1e0353882a42654b39310c4e5b270f71f6d7907f
borntofrappe <borntofrappe@gmail.com>
2020-12-06 16:47:07 +0100

    add terrain holes

--- Games/Bang Bang/Terrain/Flat line/main.lua
+++ Games/Bang Bang/Terrain/Flat line/main.lua
@@ -3,9 +3,9 @@ WINDOW_HEIGHT = 440
 POINTS = 100
 
 function love.load()
-  love.window.setTitle("Flat line bang")
+  love.window.setTitle("Flat line")
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
-  love.graphics.setBackgroundColor(1, 0.95, 1)
+  love.graphics.setBackgroundColor(1, 1, 1)
   points = {}
   for i = 1, POINTS + 1 do
     local x = (i - 1) * WINDOW_WIDTH / POINTS
@@ -23,6 +23,6 @@ end
 
 function love.draw()
   love.graphics.setLineWidth(2)
-  love.graphics.setColor(0.2, 0.2, 0.25)
+  love.graphics.setColor(0.2, 0.2, 0.22)
   love.graphics.line(points)
 end
--- Games/Bang Bang/Terrain/README.md
+++ Games/Bang Bang/Terrain/README.md
@@ -5,3 +5,7 @@ Here I explore how to create the terrain for the eventual game.
 ## Flat line
 
 Render a line by connecting a series of points. This allows to later create holes by modifying the `y` coordinates of consecutive points.
+
+## Trigonometric holes
+
+Include holes in the form of a series of points with varying `y` coordinates. The value is modified through the `sin` function and considering the range `[math.pi, 0]`, describing a counter-clockwise arc.
