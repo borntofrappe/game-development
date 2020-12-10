25762b1f991ea558c238f253c385b4193f5cd1b6
borntofrappe <borntofrappe@gmail.com>
2020-12-10 10:09:20 +0100

    add trajectory and fire to player class

--- Games/Bang Bang/src/Player.lua
+++ Games/Bang Bang/src/Player.lua
@@ -1,7 +1,12 @@
 Player = {}
 Player.__index = Player
 
-function Player:create(x, y)
+function Player:create(terrain)
+  local index = love.math.random(10) * 2 + 1
+
+  local x = terrain[index]
+  local y = terrain[index + 1]
+
   local sprite = 2
   local angle = 45
   local velocity = 80
@@ -25,30 +30,14 @@ function Player:create(x, y)
     isDestroyed = isDestroyed,
     offsetHeight = offsetHeight,
     offsetWidth = offsetWidth,
-    cannonball = cannonball
+    cannonball = cannonball,
+    terrain = terrain,
+    trajectory = {}
   }
 
   setmetatable(this, self)
 
   return this
-end
-
-function Player:update(dt)
-  if love.keyboard.isDown("down") then
-    self.angle = math.max(0, self.angle - dt * UPDATE_SPEED)
-  elseif love.keyboard.isDown("up") then
-    self.angle = math.min(90, self.angle + dt * UPDATE_SPEED)
-  end
-
-  if love.keyboard.isDown("right") then
-    self.velocity = math.min(100, self.velocity + dt * UPDATE_SPEED)
-  elseif love.keyboard.isDown("left") then
-    self.velocity = math.max(10, self.velocity - dt * UPDATE_SPEED)
-  end
-
-  if love.keyboard.wasPressed("d") then
-    self.isDestroyed = not self.isDestroyed
-  end
 end
 
 function Player:render()
@@ -84,4 +73,87 @@ function Player:render()
     )
     love.graphics.draw(gTextures["cannon"], gQuads["cannon"][1], self.x, self.y - self.offsetHeight)
   end
+end
+
+function Player:getTrajectory()
+  local xOffset = self.x + self.offsetWidth / 2
+  local yOffset = self.y - 18
+  local a = self.angle
+  local v = self.velocity
+
+  local points = {}
+  local theta = math.rad(a)
+
+  local t = 0
+  local tDelta = (self.terrain[3] - self.terrain[1]) / (v * math.cos(theta))
+
+  while true do
+    x = v * t * math.cos(theta)
+    y = v * t * math.sin(theta) - 1 / 2 * GRAVITY * t ^ 2
+
+    table.insert(points, xOffset + x)
+    table.insert(points, yOffset - y)
+    t = t + tDelta
+
+    if yOffset - y > WINDOW_HEIGHT then
+      break
+    end
+  end
+
+  return points
+end
+
+function Player:fire()
+  self.trajectory = self:getTrajectory()
+
+  local index = 1
+
+  local indexStart
+
+  for i = 1, #self.terrain, 2 do
+    if self.terrain[i] >= self.trajectory[1] then
+      indexStart = i
+      break
+    end
+  end
+
+  Timer:every(
+    2 / #self.trajectory / 2,
+    function()
+      local hasCollided = false
+
+      self.cannonball.x = self.trajectory[index]
+      self.cannonball.y = self.trajectory[index + 1]
+
+      if self.trajectory[index + 1] > self.terrain[indexStart + index + 2] then
+        hasCollided = true
+
+        local xStart = self.cannonball.x - self.cannonball.r
+        local xEnd = self.cannonball.x + self.cannonball.r
+        local angle = math.pi
+        local dAngle = math.pi / (self.cannonball.r * 2) * WINDOW_WIDTH / POINTS
+        for i = 1, #self.terrain, 2 do
+          if self.terrain[i] >= xStart and self.terrain[i] <= xEnd then
+            self.terrain[i + 1] = self.terrain[i + 1] + math.sin(angle) * self.cannonball.r
+            angle = math.max(0, angle - dAngle)
+          end
+        end
+
+        self.isFiring = false
+        self.cannonball.x = self.x + self.offsetWidth / 2
+        self.cannonball.y = self.y - 18
+        Timer:reset()
+      end
+
+      if not hasCollided then
+        index = index + 2
+        if not self.trajectory[index] then
+          self.isFiring = false
+          self.cannonball.x = self.x + self.offsetWidth / 2
+          self.cannonball.y = self.y - 18
+          Timer:reset()
+        end
+      end
+    end
+  )
 end
--- Games/Bang Bang/src/states/PlayState.lua
+++ Games/Bang Bang/src/states/PlayState.lua
@@ -1,24 +1,18 @@
 PlayState = BaseState:create()
 
 function PlayState:enter()
-  self.terrain = self:getPoints()
-
-  local xPlayer = love.math.random(10) * 2 + 1
-  self.player = Player:create(self.terrain[xPlayer], self.terrain[xPlayer + 1])
-
-  self.trajectory = self:getTrajectory(self.player)
-  self.showTrajectory = false
-  self.isFiring = false
+  self.terrain = self:getTerrain()
+  self.player = Player:create(self.terrain)
 
   self.fireButton =
     Button:create(
-    WINDOW_WIDTH / 2,
+    8,
     8,
     128,
     48,
     "Fire!",
     function()
-      self:fire()
+      self.player:fire()
     end
   )
 end
@@ -30,41 +24,38 @@ function PlayState:update(dt)
   if love.keyboard.wasPressed("escape") then
     gStateMachine:change("title")
   end
-  if love.keyboard.wasPressed("t") then
-    self.showTrajectory = not self.showTrajectory
+
+  if love.keyboard.wasPressed("return") then
+    self.player:fire()
   end
 
-  if love.keyboard.wasPressed("return") and not self.isFiring then
-    self.isFiring = true
-    self:fire()
+  if love.keyboard.isDown("down") then
+    self.player.angle = math.max(0, self.player.angle - dt * UPDATE_SPEED)
+  elseif love.keyboard.isDown("up") then
+    self.player.angle = math.min(90, self.player.angle + dt * UPDATE_SPEED)
   end
 
-  self.player:update(dt)
+  if love.keyboard.isDown("right") then
+    self.player.velocity = math.min(100, self.player.velocity + dt * UPDATE_SPEED)
+  elseif love.keyboard.isDown("left") then
+    self.player.velocity = math.max(10, self.player.velocity - dt * UPDATE_SPEED)
+  end
+
+  if love.keyboard.wasPressed("d") then
+    self.player.isDestroyed = not self.player.isDestroyed
+  end
 end
 
 function PlayState:render()
+  self.fireButton:render()
   self.player:render()
 
   love.graphics.setColor(gColors["dark"].r, gColors["dark"].g, gColors["dark"].b)
   love.graphics.setLineWidth(5)
   love.graphics.line(self.terrain)
-
-  if self.showTrajectory then
-    love.graphics.setLineWidth(2)
-    love.graphics.line(self.trajectory)
-  end
-
-  love.graphics.setFont(gFonts["normal"])
-  love.graphics.print(
-    "Angle " .. formatData(self.player.angle) .. "\nVelocity " .. formatData(self.player.velocity),
-    8,
-    8
-  )
-
-  self.fireButton:render()
 end
 
-function PlayState:getPoints()
+function PlayState:getTerrain()
   local yStart = love.math.random(math.floor(WINDOW_HEIGHT * 2 / 3), WINDOW_HEIGHT)
   local mu = love.math.random(MU_MIN, MU_MAX)
   local sigma = love.math.random(SIGMA_MIN, SIGMA_MAX)
@@ -86,87 +77,4 @@ function PlayState:getPoints()
   end
 
   return points
-end
-
-function PlayState:getTrajectory(player)
-  local xOffset = player.x + player.offsetWidth / 2
-  local yOffset = player.y - 18
-  local a = player.angle
-  local v = player.velocity
-
-  local points = {}
-  local theta = math.rad(a)
-
-  local t = 0
-  local tDelta = (self.terrain[3] - self.terrain[1]) / (v * math.cos(theta))
-
-  while true do
-    x = v * t * math.cos(theta)
-    y = v * t * math.sin(theta) - 1 / 2 * GRAVITY * t ^ 2
-
-    table.insert(points, xOffset + x)
-    table.insert(points, yOffset - y)
-    t = t + tDelta
-
-    if yOffset - y > WINDOW_HEIGHT then
-      break
-    end
-  end
-
-  return points
-end
-
-function PlayState:fire()
-  self.trajectory = self:getTrajectory(self.player)
-
-  local index = 1
-
-  local indexStart
-
-  for i = 1, #self.terrain, 2 do
-    if self.terrain[i] >= self.trajectory[1] then
-      indexStart = i
-      break
-    end
-  end
-
-  Timer:every(
-    2 / #self.trajectory / 2,
-    function()
-      local hasCollided = false
-
-      self.player.cannonball.x = self.trajectory[index]
-      self.player.cannonball.y = self.trajectory[index + 1]
-
-      if self.trajectory[index + 1] > self.terrain[indexStart + index + 2] then
-        hasCollided = true
-
-        local xStart = self.player.cannonball.x - self.player.cannonball.r
-        local xEnd = self.player.cannonball.x + self.player.cannonball.r
-        local angle = math.pi
-        local dAngle = math.pi / (self.player.cannonball.r * 2) * WINDOW_WIDTH / POINTS
-        for i = 1, #self.terrain, 2 do
-          if self.terrain[i] >= xStart and self.terrain[i] <= xEnd then
-            self.terrain[i + 1] = self.terrain[i + 1] + math.sin(angle) * self.player.cannonball.r
-            angle = math.max(0, angle - dAngle)
-          end
-        end
-
-        self.isFiring = false
-        self.player.cannonball.x = self.player.x + self.player.offsetWidth / 2
-        self.player.cannonball.y = self.player.y - 18
-        Timer:reset()
-      end
-
-      if not hasCollided then
-        index = index + 2
-        if not self.trajectory[index] then
-          self.isFiring = false
-          self.player.cannonball.x = self.player.x + self.player.offsetWidth / 2
-          self.player.cannonball.y = self.player.y - 18
-          Timer:reset()
-        end
-      end
-    end
-  )
 end
