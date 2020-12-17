eb8c1088466de9a5a92b6932803fc38534690c60
borntofrappe <borntofrappe@gmail.com>
2020-12-17 10:20:13 +0100

    [docs] introduce stencil demos

---
+++ 06 The Legend of Zelda/Stencil/README.md
@@ -0,0 +1,3 @@
+# Stencil
+
+The lecturer introduces a stencil through the `love.graphics.stencil`, `love.graphics.setStencilTest` functions. Through a couple of demos, I try to explain the concept by hiding/showing parts of a window.
---
+++ 06 The Legend of Zelda/Stencil/Show/main.lua
@@ -0,0 +1,19 @@
+-- good luck
+
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 400
+
+function love.load()
+  love.window.setTitle("Stencil — Show")
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+  love.graphics.setBackgroundColor(1, 1, 1)
+end
+
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.quit()
+  end
+end
+
+function love.draw()
+end
