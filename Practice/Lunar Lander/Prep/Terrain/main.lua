caad3b25aa86778b73f4f16041a002c62305ef17
borntofrappe <borntofrappe@gmail.com>
2021-08-03 16:05:28 +0200

    lunar lander terrain

--- Practice/Lunar Lander/Prep/Terrain/main.lua
+++ Practice/Lunar Lander/Prep/Terrain/main.lua
@@ -1,31 +1,116 @@
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 400
 
-local points = {}
+local lines
+local index
 
 function love.load()
   love.window.setTitle("Terrain")
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
   love.graphics.setBackgroundColor(0.12, 0.12, 0.12)
 
-  local n = 100
-  local y = WINDOW_HEIGHT / 2
-  for i = 1, n + 1 do
-    local x = (i - 1) * WINDOW_WIDTH / n
-    table.insert(points, x)
-    table.insert(points, y)
+  local baseline = {
+    ["keyword"] = "baseline",
+    ["points"] = {}
+  }
+  local random = {
+    ["keyword"] = "random",
+    ["points"] = {}
+  }
+  local noise = {
+    ["keyword"] = "noise",
+    ["points"] = {}
+  }
+  local platform = {
+    ["keyword"] = "platform",
+    ["points"] = {}
+  }
+
+  local points = 100
+
+  local yBaseline = WINDOW_HEIGHT * 3 / 4
+  local yGap = WINDOW_HEIGHT / 2.5
+
+  local offsetNoise = love.math.random(1000)
+  local offsetIncrement = 0.15
+
+  local offsetNoisePlatform = offsetNoise
+
+  local pointsPerPlatform = math.ceil(points / WINDOW_WIDTH * 25) -- at least 20px wide
+  local platforms = 3
+  local platformsStart = {}
+
+  repeat
+    local platformStart = love.math.random(points - pointsPerPlatform * platforms)
+    local overlapsPlatformStart = false
+    for _, point in ipairs(platformsStart) do
+      if platformStart >= point and platformStart < point + pointsPerPlatform then
+        overlapsPlatformStart = true
+        break
+      end
+    end
+    if not overlapsPlatformStart then
+      table.insert(platformsStart, platformStart)
+      platforms = platforms - 1
+    end
+  until platforms == 0
+
+  local pointsPlatforms = {}
+  for _, platformStart in ipairs(platformsStart) do
+    for point = platformStart, platformStart + pointsPerPlatform do
+      pointsPlatforms[point] = true
+    end
   end
+
+  for point = 1, points + 1 do
+    local x = (point - 1) * WINDOW_WIDTH / points
+    local yRandom = yBaseline + love.math.random() * yGap - yGap / 2
+    local yNoise = yBaseline + love.math.noise(offsetNoise) * yGap - yGap / 2
+    local yPlatform = yBaseline + love.math.noise(offsetNoisePlatform) * yGap - yGap / 2
+
+    table.insert(baseline.points, x)
+    table.insert(random.points, x)
+    table.insert(noise.points, x)
+    table.insert(platform.points, x)
+
+    table.insert(baseline.points, yBaseline)
+    table.insert(random.points, yRandom)
+    table.insert(noise.points, yNoise)
+    table.insert(platform.points, yPlatform)
+
+    offsetNoise = offsetNoise + offsetIncrement
+
+    if not pointsPlatforms[point] then
+      offsetNoisePlatform = offsetNoisePlatform + offsetIncrement
+    end
+  end
+
+  lines = {}
+  table.insert(lines, baseline)
+  table.insert(lines, random)
+  table.insert(lines, noise)
+  table.insert(lines, platform)
+  index = 1
 end
 
 function love.keypressed(key)
   if key == "escape" then
     love.event.quit()
   end
+
+  if key == "right" then
+    index = index == #lines and 1 or index + 1
+  end
+
+  if key == "left" then
+    index = index == 1 and #lines or index - 1
+  end
 end
 
 function love.draw()
   love.graphics.setColor(0.95, 0.95, 0.95)
-  love.graphics.setLineWidth(2)
+  love.graphics.printf(lines[index].keyword:upper(), 0, WINDOW_HEIGHT / 4 - 4, WINDOW_WIDTH, "center")
 
-  love.graphics.line(points)
+  love.graphics.setLineWidth(2)
+  love.graphics.line(lines[index].points)
 end
--- Practice/Lunar Lander/README.md
+++ Practice/Lunar Lander/README.md
@@ -4,4 +4,48 @@ Safely land a module on one of the platforms intersposed between jagged surface.
 
 ## Prep
 
-The `Prep` folder includes a series of smaller demos to independently work on the game's main features. In `Terrain` I lean on random functions to produce the jagged surface. In `Box2D Module` I introduce the lander with the physics library and as a series of shapes attached to the same module.
+The `Prep` folder includes a series of smaller demos to independently work on the game's main features.
+
+### Terrain
+
+The goal is to show how to create uneven, smooth terrain with platforms, relying on the random functions provided by Love2D. The demo plots different lines, leading up to the formula ultimately used in the project:
+
+1. baseline, where the vertical coordinate does not change
+
+2. random, where the `y` coordinate is computed through `love.math.random`. The solution is random, but excessively so
+
+3. noise, where the same value is computed through `love.math.noise`, incrementing the offset passed to the function at each iteration
+
+   ```lua
+   love.math.noise(offsetNoise)
+
+   offsetNoise = offsetNoise + offsetIncrement
+   ```
+
+   The greater the increment, the more the function resembles `love.math.random`. The smaller the increment, the smoother the change in value. It is worth noting that the function returns the same value for the same offset, which becomes exceedingly useful considering the final line
+
+4. platform, where the line uses `love.math.noise`, but does not increment the offset for a selection of points. In so doing, the vertical coordinate remains the same and the line produces the desired, even surface suitable for a soft landing
+
+   ```lua
+   love.math.noise(offsetNoisePlatform)
+
+   if platform then --pseudocode
+     offsetNoisePlatform = offsetNoisePlatform + offsetIncrement
+   end
+   ```
+
+   In practice, the code highlights a platform with a table populated with the points which need to be skipped.
+
+   ```lua
+   pointsPlatforms = {
+     [3] = true,
+     [4] = true,
+     -- ...
+   }
+   ```
+
+Press the left and right arrow keys to move between the different options.
+
+### Box2D Module
+
+The goal is to build a basic Box2D simulation to showcase how the lander should look and behave.
