62d53d0f64b14cf5396d9432af5626355543c784
borntofrappe <borntofrappe@gmail.com>
2021-08-14 23:16:14 +0200

    prep line drawing points

--- Practice/Missile Command/Prep/Line Points/main.lua
+++ Practice/Missile Command/Prep/Line Points/main.lua
@@ -1,6 +1,9 @@
+Timer = require "Timer"
+
 local WINDOW_WIDTH = 420
 local WINDOW_HEIGHT = 380
 local UPDATE_SPEED = 70
+local RESOLUTION = 5
 
 local source = {
   ["x"] = WINDOW_WIDTH / 2,
@@ -16,7 +19,7 @@ local target = {
 local points = {}
 
 function love.load()
-  love.window.setTitle("Line Vertices")
+  love.window.setTitle("Line Points")
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
   love.graphics.setBackgroundColor(1, 1, 1)
 end
@@ -27,11 +30,42 @@ function love.keypressed(key)
   end
 
   if key == "return" then
-    points = {source.x, source.y, target.x, target.y}
+    local dx = (target.x - source.x) / RESOLUTION
+    local dy = (target.y - source.y) / RESOLUTION
+
+    local nPts = math.max(math.abs(dx), math.abs(dy))
+
+    local pts = {}
+
+    local x = source.x
+    local y = source.y
+
+    for i = 1, nPts + 1 do
+      table.insert(pts, x + math.floor(i * dx) / nPts * RESOLUTION)
+      table.insert(pts, y + math.floor(i * dy) / nPts * RESOLUTION)
+    end
+
+    Timer:reset()
+    local index = 0
+    Timer:every(
+      1 / nPts,
+      function()
+        index = index + 2
+        if index >= #pts then
+          Timer:reset()
+        else
+          points = {}
+          for i = 1, index do
+            table.insert(points, pts[i])
+          end
+        end
+      end
+    )
   end
 end
 
 function love.update(dt)
+  Timer:update(dt)
   if love.keyboard.isDown("up") then
     target.y = target.y - UPDATE_SPEED * dt
   elseif love.keyboard.isDown("down") then
@@ -54,6 +88,7 @@ function love.draw()
   love.graphics.line(target.x, target.y - target.size, target.x, target.y + target.size)
 
   if #points > 2 then
+    -- love.graphics.print(#points, 8, 8)
     love.graphics.setLineWidth(0.5)
     love.graphics.line(points)
   end
